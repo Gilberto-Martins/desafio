df13f459a4bde6ecab96ce99d9d8d37a341f3383
Gilberto Martins <gilbertomartins643@gmail.com>
2021-01-08 10:04:42 -0300

    desfio false

--- desafio1.js
+++ desafio1.js
@@ -1,16 +1,17 @@
 
-
-console.log("Digite dois valores: ");
-let numb1 = parseInt(gets());
-let numb2 = parseInt(gets());
-
-let div = numb1 / numb2;
-
-console.log(div);
-
+/*
+let numb1 =20;
+let numb2 = 10;
+let dive = numb1 / numb2;
+console.log(`O resutado foi ${dive}`);
+console.log(`O primeiro valor foi: ${numb1} e o segundo valor foi ${numb2}`)
+*/
 
 
 /*
+
+não esta funcionando o codigo da divisão
+
 let limit = parseInt(gets());
 
 for (let i = 0; i < limit; i++) {
@@ -27,3 +28,25 @@ for (let i = 0; i < limit; i++) {
     }
 }
 */
+
+const pessoa = {
+    nome: 'Ana',
+    idade: 21,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 1000
+    }
+}
+
+
+const { nome, idade } = pessoa
+console.log(nome , idade)
+
+const { nome: n, idade: i} = pessoa
+console.log(n, i)
+
+const { bemHumorada = true, sobrenome} = pessoa
+console.log(bemHumorada, sobrenome)
+
+const { endereco:{ logradouro , numero}} = pessoa
+console.log(logradouro , numero)
